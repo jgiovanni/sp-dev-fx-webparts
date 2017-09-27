289c2e2e47d5898bab830fe1a0028c0e613c8a95
Jerez Bain <jerezb31@gmail.com>
2017-09-27 17:57:38 -0400

    upload deploy files for cdn test

---
+++ samples/vuejs-todo-single-file-component/deploy/vuejs-todo-single-file-component-stagemapstrings_en-us_fcb9c2e9c1849393569b2dddbb5471f0.js
@@ -0,0 +1,7 @@
+define([], function() {
+    return {
+        "PropertyPaneDescription": "OrangeGate Stage Map",
+        "BasicGroupName": "Title",
+        "DescriptionFieldLabel": "Change the title"
+    }
+});
